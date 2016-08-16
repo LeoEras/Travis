fd4a9b778b56310d3f40a6c8a8cd479da2411238
Leonardo <leoded94@hotmail.com>
2016-08-16 15:01:56 -0500

    Agregado test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+import unittest
+from programa import *
+
+class SimpleTestCase(unittest.TestCase):
+    def testA(self):
+        libro = Libro("CE", 0)
+        assert libro.prestamo("10/02/2015") == "17/02/2015"
+
+    def testB(self):
+        libro = Libro("CH", 0)
+        assert libro.prestamo("10/02/2015") == "24/02/2015"
+
+    def testC(self):
+        libro = Libro("CE", 1)
+        assert libro.prestamo("10/02/2015") == 0
+
+    def testD(self):
+        libro = Libro("CN", 1)
+        assert libro.prestamo("10/02/2015") == 0
+
+    def testE(self):
+        libro = Libro("AD", 1)
+        assert libro.prestamo("10/02/2015") == 0
+        
+if __name__ == "__main__":
+    unittest.main()
